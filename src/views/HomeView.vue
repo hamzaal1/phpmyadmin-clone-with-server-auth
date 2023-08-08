eddf0ddc94dfc81842dc4df7e682d2c3ebc92339
hamza <hamzademnati21@gmail.com>
2023-08-08 15:26:24 +0100

    phpmyadmin new clone

---
+++ src/views/HomeView.vue
@@ -0,0 +1,92 @@
+<script setup>
+</script>
+
+<template>
+  <div id="page-container">
+    <!-- Main Container -->
+    <main id="main-container">
+      <!-- Page Content -->
+      <div class="hero-static d-flex align-items-center">
+        <div class="content">
+          <div class="row justify-content-center push">
+            <div class="col-md-8 col-lg-6 col-xl-4">
+              <!-- Sign In Block -->
+              <div class="block block-rounded mb-0">
+                <div class="block-header block-header-default">
+                  <h3 class="block-title">Sign In</h3>
+                </div>
+                <div class="block-content">
+                  <div class="p-sm-3 px-lg-4 px-xxl-5 py-lg-5">
+                    <h1 class="h2 mb-1">PhpMyAdmin Clone</h1>
+                    <p class="fw-medium text-muted">Welcome, please login.</p>
+                    <form
+                      class="js-validation-signin"
+                      action="be_pages_auth_all.html"
+                      method="POST"
+                    >
+                      <div class="py-3">
+                        <div class="mb-4">
+                          <input
+                            type="text"
+                            class="form-control form-control-alt form-control-lg"
+                            id="login-ip"
+                            name="login-ip"
+                            placeholder="Host IP"
+                          />
+                        </div>
+                        <div class="mb-4">
+                          <input
+                            type="text"
+                            class="form-control form-control-alt form-control-lg"
+                            id="login-username"
+                            name="login-username"
+                            placeholder="Username"
+                          />
+                        </div>
+                        <div class="mb-4">
+                          <input
+                            type="password"
+                            class="form-control form-control-alt form-control-lg"
+                            id="login-password"
+                            name="login-password"
+                            placeholder="Password"
+                          />
+                        </div>
+                        <div class="mb-4">
+                          <div class="form-check">
+                            <input
+                              class="form-check-input"
+                              type="checkbox"
+                              value=""
+                              id="login-remember"
+                              name="login-remember"
+                            />
+                            <label class="form-check-label" for="login-remember">Remember Me</label>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="row mb-4">
+                        <div class="col-md-6 col-xl-5">
+                          <button type="submit" class="btn w-100 btn-alt-primary">
+                            <i class="fa fa-fw fa-sign-in-alt me-1 opacity-50"></i> Sign In
+                          </button>
+                        </div>
+                      </div>
+                    </form>
+                    <!-- END Sign In Form -->
+                  </div>
+                </div>
+              </div>
+              <!-- END Sign In Block -->
+            </div>
+          </div>
+          <div class="fs-sm text-muted text-center">
+            <strong>TechStylitix Clones</strong> &copy; <span data-toggle="year-copy"></span>
+          </div>
+        </div>
+      </div>
+      <!-- END Page Content -->
+    </main>
+    <!-- END Main Container -->
+  </div>
+</template>
